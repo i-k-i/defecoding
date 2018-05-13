eaaf75afa0be82f3820ea7757054cb6b47600b06
Leo5700 <3900000@gmail.com>
2018-05-14 02:27:58 +0300

    Update and rename p42.py to reprint.py

--- reprint.py
+++ reprint.py
@@ -7,17 +7,15 @@
 0.03508771929824561
 
 Я написал функцию, которая избавляет Вас от необходимости дважды писать одно и то же:
-
 >>> print(p42(), f(x, 57))
 f(x, 57) = 0.03508771929824561
 
 Есть ещё функция p43, которая делает так:
-
 >>> p43('f(x, 57)')
 f(x, 57) = 0.03508771929824561
 
-А функция p44 ващще делает то, что надо:
->>> p44(f(x, 57))
+А функция reprint ващще делает то, что надо:
+>>> reprint(f(x, 57))
 f(x, 57) = 0.03508771929824561
 
 
@@ -60,14 +58,13 @@ def p43(code):
 p43('f(x, 57)')
 
 
-def p44(*args):
+def reprint(*args):
     lineno = inspect.currentframe().f_back.f_lineno
     currentscriptath = os.path.realpath(__file__)
     with open(currentscriptath, 'r', encoding='utf') as file:
         codeline = file.readlines()[lineno - 1]
-    print(codeline)
-    codetoeval = codeline[4:][:-2]
-    container = codetoeval + ' ='
-    print(container, eval(codetoeval))
+    codetoeval = ''.join(codeline.split('(', 1)[1:])[:-2]
+    print(codetoeval, '=', eval(codetoeval))
 
-p44(f(x, 57))
+reprint(f(x, 57))
+reprint(f(x, 57), (42, 57))
