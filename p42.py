2e780e38b8891d0e373538e60ca37ca4a9df486a
Leo5700 <3900000@gmail.com>
2018-05-14 02:13:08 +0300

    Update p42.py

--- p42.py
+++ p42.py
@@ -1,7 +1,7 @@
 # python3
 
 '''
-Чтобы избежать в математических расчётах конструкции типа:
+Чтобы избежать (в математических расчётах) конструкции типа:
 
 >>> print('f(x, 57))', '=', f(x, 57)))
 0.03508771929824561
@@ -10,6 +10,16 @@
 
 >>> print(p42(), f(x, 57))
 f(x, 57) = 0.03508771929824561
+
+Есть ещё функция p43, которая делает так:
+
+>>>> p43('f(x, 57)')
+f(x, 57) = 0.03508771929824561
+
+А функция p44 ващще делает то, что надо:
+>>>> p44(f(x, 57))
+f(x, 57) = 0.03508771929824561
+
 
 Sourse:
 https://gisstudio.wordpress.com/2013/04/30/an-easy-way-to-get-the-line-number-of-your-python-code/
@@ -48,3 +58,16 @@ def p43(code):
     print(code, '=', eval(code))
 
 p43('f(x, 57)')
+
+
+def p44(*args):
+    lineno = inspect.currentframe().f_back.f_lineno
+    currentscriptath = os.path.realpath(__file__)
+    with open(currentscriptath, 'r', encoding='utf') as file:
+        codeline = file.readlines()[lineno - 1]
+    print(codeline)
+    codetoeval = codeline[4:][:-2]
+    container = codetoeval + ' ='
+    print(container, eval(codetoeval))
+
+p44(f(x, 57))
